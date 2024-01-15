5f604c0e0ae2f624370beaf52b5ad9bd1ab6c576
Shakhawat Bijoy <piyash2003b@gmail.com>
2024-01-15 10:51:02 +0600

    modified

--- main.c
+++ main.c
@@ -47,6 +47,58 @@ int main()
 }
     return 0;
 }
+
+node *main_logs(node *start)
+{
+    system("cls");
+    printf("\n\n\n");
+    printf("        \t\t\t1. FOR VOTE ENTRY     \n");
+    Sleep(300);
+    printf("        \t\t\t2. FOR ADMIN PANEL    \n");
+    Sleep(300);
+    printf("        \t\t\t3. FOR WINNER         \n");
+    printf("    \t IF YOUR CREDENTIALS MATCHES WITH THOSE IN THE VOTER LIST THEN ONLY YOU CAN GIVE YOUR VOTE OTHERWISE YOU CAN NOT    \n\n\n");
+    Sleep(500);
+    printf("    \t ********So Please Enter********\n\n\n");
+    int T;
+    scanf("%d",&T);
+    if(T==1)
+    {
+       start= voter_insert(start);
+    }
+    if(T==2)
+    {
+      admin();
+    }
+    if(T==3)
+    {
+      winner();
+    }
+    if(T!=1||T!=2||T!=3)
+    {
+    	main_logs(start);
+	}
+
+
+    return start;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // git remote add origin https://github.com/luciiifeer/Online-Voting-System-Data-Structure-Project.git
 // git branch -M main
 // git push -u origin main
