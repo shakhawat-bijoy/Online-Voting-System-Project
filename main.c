09babcad4ed25cfdc7b6426b9493614773e7fa7c
Shakhawat Bijoy <piyash2003b@gmail.com>
2024-01-28 23:47:57 +0600

    updated

--- main.c
+++ main.c
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-=======
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct voter_information
-{
-    char nid[20];
-    char name[50];
-    char birth_date[15];
-    struct voter_information *next;
-} node;
-
-node *head;
-node *space,*start=NULL;
-node *search(char x[],char y[],char z[],node *,int *);
-node *voter_insert(node *);
-node *main_logs(node *);
- void admin();
- void winner();
- 	void exi();
- 	void voting();
- 	void not_again();
- 	void stop();
- 	void show();
-
-
-
-
->>>>>>> e9bb4393143e4dff6384e5fdc352fa10f9df35d4
-
-
 
 
 
